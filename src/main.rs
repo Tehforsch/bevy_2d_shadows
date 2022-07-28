b01dd52f74e128f2674fd0ed05707dc925e58090
tehforsch <mail@tonipeter.de>
2022-07-28 22:45:45 +0200

    really dumb commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
+use bevy::core_pipeline::draw_2d_graph;
+use bevy::core_pipeline::node;
+use bevy::core_pipeline::RenderTargetClearColors;
+use bevy::core_pipeline::Transparent2d;
 use bevy::ecs::system::lifetimeless::SRes;
 use bevy::ecs::system::SystemParamItem;
 use bevy::prelude::shape::Quad;
 use bevy::prelude::*;
 use bevy::reflect::TypeUuid;
+use bevy::render::camera::ActiveCamera;
+use bevy::render::camera::CameraProjection;
+use bevy::render::camera::CameraTypePlugin;
+use bevy::render::camera::DepthCalculation;
+use bevy::render::camera::RenderTarget;
+use bevy::render::primitives::Frustum;
 use bevy::render::render_asset::PrepareAssetError;
 use bevy::render::render_asset::RenderAsset;
 use bevy::render::render_asset::RenderAssets;
+use bevy::render::render_graph::Node;
+use bevy::render::render_graph::NodeRunError;
+use bevy::render::render_graph::RenderGraph;
+use bevy::render::render_graph::RenderGraphContext;
+use bevy::render::render_graph::SlotValue;
+use bevy::render::render_phase::RenderPhase;
 use bevy::render::render_resource::std140::AsStd140;
 use bevy::render::render_resource::std140::Std140;
 use bevy::render::render_resource::BindGroup;
@@ -29,12 +45,20 @@ use bevy::render::render_resource::TextureFormat;
 use bevy::render::render_resource::TextureSampleType;
 use bevy::render::render_resource::TextureUsages;
 use bevy::render::render_resource::TextureViewDimension;
+use bevy::render::renderer::RenderContext;
 use bevy::render::renderer::RenderDevice;
+use bevy::render::view::RenderLayers;
+use bevy::render::view::VisibleEntities;
+use bevy::render::RenderApp;
+use bevy::render::RenderStage;
 use bevy::sprite::Material2d;
 use bevy::sprite::Material2dPipeline;
 use bevy::sprite::Material2dPlugin;
 use bevy::sprite::MaterialMesh2dBundle;
 use bevy::sprite::Mesh2dHandle;
+
+// The name of the final node of the first pass.
+pub const FIRST_PASS_DRIVER: &str = "first_pass_driver";
 
 #[derive(AsStd140, Clone, Debug, Default)]
 pub struct MyData {
@@ -190,12 +214,53 @@ impl Material2d for MyMaterial {
     }
 }
 
+#[derive(Component)]
+struct FirstPassObject;
+
+#[derive(Component, Default)]
+pub struct FirstPassCamera;
+
+pub fn new_2d(render_target: Handle<Image>) -> OrthographicCameraBundle<FirstPassCamera> {
+    // we want 0 to be "closest" and +far to be "farthest" in 2d, so we offset
+    // the camera's translation by far and use a right handed coordinate system
+    let far = 1000.0;
+    let orthographic_projection = OrthographicProjection {
+        far,
+        depth_calculation: DepthCalculation::ZDifference,
+        ..Default::default()
+    };
+    let transform = Transform::from_xyz(0.0, 0.0, far - 0.1);
+    let view_projection =
+        orthographic_projection.get_projection_matrix() * transform.compute_matrix().inverse();
+    let frustum = Frustum::from_view_projection(
+        &view_projection,
+        &transform.translation,
+        &transform.back(),
+        orthographic_projection.far(),
+    );
+    OrthographicCameraBundle {
+        camera: Camera {
+            near: orthographic_projection.near,
+            far: orthographic_projection.far,
+            target: RenderTarget::Image(render_target),
+            ..Default::default()
+        },
+        orthographic_projection,
+        visible_entities: VisibleEntities::default(),
+        frustum,
+        transform,
+        global_transform: Default::default(),
+        marker: FirstPassCamera,
+    }
+}
+
 fn setup(
     mut commands: Commands,
     mut custom_materials: ResMut<Assets<MyMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
     asset_server: Res<AssetServer>,
     mut images: ResMut<Assets<Image>>,
+    mut clear_colors: ResMut<RenderTargetClearColors>,
 ) {
     asset_server.watch_for_changes().unwrap();
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
@@ -210,7 +275,7 @@ fn setup(
             label: None,
             size,
             dimension: TextureDimension::D2,
-            format: TextureFormat::R8Unorm,
+            format: TextureFormat::Rgba8Unorm,
             mip_level_count: 1,
             sample_count: 1,
             usage: TextureUsages::TEXTURE_BINDING
@@ -235,21 +300,128 @@ fn setup(
 
     let light_map_handle = images.add(light_map);
 
-    let mesh = Mesh::from(Quad::new(Vec2::new(600.0, 400.0)));
-    commands.spawn_bundle(MaterialMesh2dBundle {
-        mesh: Mesh2dHandle(meshes.add(mesh)),
-        material: custom_materials.add(MyMaterial::new(
-            asset_server.load("tree.png"),
-            light_map_handle,
-        )),
+    // first pass stuff
+    // This specifies the layer used for the first pass, which will be attached to the first pass camera and cube.
+    let first_pass_layer = RenderLayers::layer(1);
+
+    // The cube that will be rendered to the texture.
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: asset_server.load("box.png"),
+            ..default()
+        })
+        .insert(FirstPassObject)
+        .insert(first_pass_layer);
+
+    // Light
+    // NOTE: Currently lights are shared between passes - see https://github.com/bevyengine/bevy/issues/3462
+    commands.spawn_bundle(PointLightBundle {
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 10.0)),
         ..default()
     });
+
+    // First pass camera
+    let render_target = RenderTarget::Image(light_map_handle.clone());
+    clear_colors.insert(render_target.clone(), Color::WHITE);
+    commands
+        .spawn_bundle(new_2d(light_map_handle.clone()))
+        .insert(first_pass_layer);
+    // NOTE: omitting the RenderLayers component for this camera may cause a validation error:
+    //
+    // thread 'main' panicked at 'wgpu error: Validation Error
+    //
+    //    Caused by:
+    //        In a RenderPass
+    //          note: encoder = `<CommandBuffer-(0, 1, Metal)>`
+    //        In a pass parameter
+    //          note: command buffer = `<CommandBuffer-(0, 1, Metal)>`
+    //        Attempted to use texture (5, 1, Metal) mips 0..1 layers 0..1 as a combination of COLOR_TARGET within a usage scope.
+    //
+    // This happens because the texture would be written and read in the same frame, which is not allowed.
+    // So either render layers must be used to avoid this, or the texture must be double buffered.
+
+    // second pass stuff
+    let mesh = Mesh::from(Quad::new(Vec2::new(600.0, 400.0)));
+    // commands.spawn_bundle(MaterialMesh2dBundle {
+    //     mesh: Mesh2dHandle(meshes.add(mesh)),
+    //     material: custom_materials.add(MyMaterial::new(
+    //         asset_server.load("tree.png"),
+    //         light_map_handle,
+    //     )),
+    //     ..default()
+    // });
 }
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins)
         .add_plugin(Material2dPlugin::<MyMaterial>::default())
-        .add_startup_system(setup)
-        .run();
+        .add_plugin(CameraTypePlugin::<FirstPassCamera>::default())
+        .add_startup_system(setup);
+
+    let render_app = app.sub_app_mut(RenderApp);
+    let driver = FirstPassCameraDriver::new(&mut render_app.world);
+    render_app.add_system_to_stage(RenderStage::Extract, extract_first_pass_camera_phases);
+
+    let mut graph = render_app.world.resource_mut::<RenderGraph>();
+
+    // Add a node for the first pass.
+    graph.add_node(FIRST_PASS_DRIVER, driver);
+
+    // The first pass's dependencies include those of the main pass.
+    graph
+        .add_node_edge(node::MAIN_PASS_DEPENDENCIES, FIRST_PASS_DRIVER)
+        .unwrap();
+
+    // Insert the first pass node: CLEAR_PASS_DRIVER -> FIRST_PASS_DRIVER -> MAIN_PASS_DRIVER
+    graph
+        .add_node_edge(node::CLEAR_PASS_DRIVER, FIRST_PASS_DRIVER)
+        .unwrap();
+    graph
+        .add_node_edge(FIRST_PASS_DRIVER, node::MAIN_PASS_DRIVER)
+        .unwrap();
+
+    app.run();
+}
+
+fn extract_first_pass_camera_phases(
+    mut commands: Commands,
+    active: Res<ActiveCamera<FirstPassCamera>>,
+) {
+    if let Some(entity) = active.get() {
+        commands
+            .get_or_spawn(entity)
+            .insert_bundle((RenderPhase::<Transparent2d>::default(),));
+    }
+}
+
+struct FirstPassCameraDriver {
+    query: QueryState<Entity, With<FirstPassCamera>>,
+}
+
+impl FirstPassCameraDriver {
+    pub fn new(render_world: &mut World) -> Self {
+        Self {
+            query: QueryState::new(render_world),
+        }
+    }
+}
+
+impl Node for FirstPassCameraDriver {
+    fn update(&mut self, world: &mut World) {
+        self.query.update_archetypes(world);
+    }
+
+    fn run(
+        &self,
+        graph: &mut RenderGraphContext,
+        _render_context: &mut RenderContext,
+        world: &World,
+    ) -> Result<(), NodeRunError> {
+        for camera in self.query.iter_manual(world) {
+            dbg!("No idea what this means");
+            graph.run_sub_graph(draw_2d_graph::NAME, vec![SlotValue::Entity(camera)])?;
+        }
+        Ok(())
+    }
 }
