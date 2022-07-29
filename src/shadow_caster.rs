918fca2762923b9386c27da60099487909dae031
tehforsch <mail@tonipeter.de>
2022-07-29 16:11:45 +0200

    add really sloppy calculation of 'extreme points' of mesh

--- src/shadow_caster.rs
+++ src/shadow_caster.rs
@@ -1,11 +1,14 @@
 use std::collections::HashMap;
 
 use bevy::math::Vec3Swizzles;
+use bevy::math::Vec4Swizzles;
 use bevy::prelude::shape::Quad;
 use bevy::prelude::*;
 use bevy::render::mesh::Indices;
 use bevy::render::mesh::PrimitiveTopology;
+use bevy::render::mesh::VertexAttributeValues;
 use bevy::sprite::Mesh2dHandle;
+use ordered_float::OrderedFloat;
 
 use crate::light::Light;
 use crate::shadow_pass::LIGHT_PASS_LAYER;
@@ -36,19 +39,19 @@ pub fn spawn_shadows_system(
     mut shadow_casters: Query<(&mut ShadowCaster, &Mesh2dHandle, &Transform)>,
     _shadows: Query<&Shadow>,
 ) {
-    for (mut shadow_caster, _mesh, caster_transform) in shadow_casters.iter_mut() {
+    for (mut shadow_caster, shadow_caster_mesh_handle, shadow_caster_transform) in
+        shadow_casters.iter_mut()
+    {
         for (light_entity, light_transform, _) in lights.iter() {
             match shadow_caster.shadows.get(&light_entity) {
                 Some(_shadow) => adjust_shadow_mesh(),
                 None => {
-                    let size = 50.0;
                     let light_pos = light_transform.translation.xy();
-                    let caster_pos = caster_transform.translation.xy();
-                    let dir = (light_pos - caster_pos).normalize();
-                    let normal = Vec2::new(-dir.y, dir.x);
-                    let a = caster_pos + normal * size;
-                    let b = caster_pos - normal * size;
-                    let handle = meshes.add(get_mesh(light_pos, a, b));
+                    let shadow_caster_mesh: &Mesh =
+                        meshes.get(shadow_caster_mesh_handle.0.clone()).unwrap();
+                    let shadow_mesh =
+                        get_shadow_mesh(light_pos, &shadow_caster_mesh, shadow_caster_transform);
+                    let handle = meshes.add(shadow_mesh);
                     let mat = materials.add(ColorMaterial {
                         color: Color::BLUE,
                         ..Default::default()
@@ -69,15 +72,38 @@ pub fn spawn_shadows_system(
     }
 }
 
-fn get_mesh(light_pos: Vec2, a: Vec2, b: Vec2) -> Mesh {
-    let size = 150.0;
-    let extent_x = size / 2.0;
-    let extent_y = size / 2.0;
+fn get_shadow_mesh(
+    light_pos: Vec2,
+    shadow_caster_mesh: &Mesh,
+    shadow_caster_transform: &Transform,
+) -> Mesh {
+    let points = shadow_caster_mesh
+        .attribute(Mesh::ATTRIBUTE_POSITION)
+        .unwrap();
+    let get_angle = |point: &Vec4| -> OrderedFloat<f32> {
+        let dist = point.xy() - light_pos;
+        OrderedFloat(dist.x.atan2(dist.y))
+    };
+    let matrix = shadow_caster_transform.compute_matrix();
+    if let VertexAttributeValues::Float32x3(points) = points {
+        let transformed = points
+            .iter()
+            .map(|point| matrix * Vec4::new(point[0], point[1], 0.0, 1.0));
+        let a = transformed.max_by_key(get_angle).unwrap();
+        let transformed = points
+            .iter()
+            .map(|point| matrix * Vec4::new(point[0], point[1], 0.0, 1.0));
+        let b = transformed.min_by_key(get_angle).unwrap();
+        get_infinity_projection_quad(light_pos, Vec2::new(a[0], a[1]), Vec2::new(b[0], b[1]))
+    } else {
+        unreachable!()
+    }
+}
 
+fn get_infinity_projection_quad(light_pos: Vec2, a: Vec2, b: Vec2) -> Mesh {
     // We don't take infinity all that literally here. Gotta move this to homogeneous coordinates if I ever understand how to
-    dbg!(light_pos, a, b);
-    let a_infinity = light_pos + (a - light_pos) * 2.0;
-    let b_infinity = light_pos + (b - light_pos) * 2.0;
+    let a_infinity = light_pos + (a - light_pos) * 100.0;
+    let b_infinity = light_pos + (b - light_pos) * 100.0;
 
     let (u_left, u_right) = (1.0, 0.0);
     let vertices = [
@@ -86,7 +112,6 @@ fn get_mesh(light_pos: Vec2, a: Vec2, b: Vec2) -> Mesh {
         (b_infinity, [0.0, 0.0, 1.0], [u_right, 0.0]),
         (a_infinity, [0.0, 0.0, 1.0], [u_right, 1.0]),
     ];
-    dbg!(vertices);
 
     let indices = Indices::U32(vec![0, 2, 1, 0, 3, 2]);
 
